e39d0d1a553dfb4354f36350e88ebbd61ebdc852
Chirag gour <chiragvgour@gmail.com>
2023-09-21 05:50:30 -0700

    New Changes

--- routes/book.js
+++ routes/book.js
@@ -38,30 +38,72 @@ router.post('/addbooks', userdata, [//below are the check we use for validating 
         }
     })
 
-//ROUTE 3 : updat an existing book using put"/updatebook".login required
+//ROUTE 3 : update an existing book using put"/updatebook".login required
+//for updation we use 'put'
 router.put('/updatebook/:id', userdata, async (req, res) => {
-    const { title, description, link, tag } = req.body;
-    //creating an updatebook object
-    const updateBook = {};
-    if (title) {
-        updateBook.title = title;
-    };
-    if (description) {
-        updateBook.description = description;
-    };
-    if (link) {
-        updateBook.link = link;
-    };
-    if (title) {
-        updateBook.tag = tag;
-    };
-    // first we will find and check the book in database to be updated and update it
-    const book = Book.findById(req.params.id);
-    // if it is not there then 404 status will be send with not found message
-    if(!book){ return res.status(404).send("Not Found")};
-    if (book.user.toString() !== req.user.id) {
-        return res.status(404).send("Not Found")
+    try {
+        const { title, description, link, tag } = req.body;
+        //creating an updatebook object
+        const updateBook = {};
+        if (title) {
+            updateBook.title = title;
+        };
+        if (description) {
+            updateBook.description = description;
+        };
+        if (link) {
+            updateBook.link = link;
+        };
+        if (title) {
+            updateBook.tag = tag;
+        };
+
+        // first we will find and check the book in database to be updated and update it
+        let book = await Book.findById(req.params.id);
+
+        // if it is not there then 404 status will be send with not found message
+        if (!book) { return res.status(404).send("Not Found") };
+        //to check book which is edited by the user is there's or not if it's not there's then status 401 is send with not allowed message
+        if (book.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+
+        // now we will update the book after all checks of owner of book i.e., user
+        book = await Book.findByIdAndUpdate(req.params.id, { $set: updateBook }, { new: true })
+        res.json(book);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("There is some Internal Server Error")
     }
-    res.json(book);
+
 })
+
+//ROUTE 4 : delete an existing book using delete"/updatebook".login required
+//for updation we use 'put'
+router.delete('/deletebook/:id', userdata, async (req, res) => {
+
+    try {
+        // first we will find and check the book in database to be delete it
+        let book = await Book.findById(req.params.id);
+
+        // if it is not there then 404 status will be send with not found message
+        if (!book) { return res.status(404).send("Not Found") };
+
+        //to check book which is edited by the user is there's or not if it's not there's then status 401 is send with not allowed message
+        // allow deletion only if user owns this book and account 
+        if (book.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+
+        //deleting the book
+        book = await Book.findByIdAndDelete(req.params.id)
+        res.json({ "success": "book has been deleted", book: book });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("There is some Internal Server Error")
+    }
+
+})
+
+
 module.exports = router
